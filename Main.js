eb0ff28508568325b347621219c9d8982f28c55c
Isaiah LaDuke <iladuke@atsapllc.com>
2024-02-27 09:59:11 -0500

    feat: completing the CRUD operations for this blog post website

--- Main.js
+++ Main.js
@@ -1,1 +1,11 @@
-const colorThemse = document.querySelector
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+const storeTheme = function(theme) {
+    localStorage.setItem("theme", theme)
+}
+
+colorThemes.forEach(themeOption => {
+    themeOption.addEventListener('click', () => {
+        storeTheme(themeOption.id);
+    })
+})
